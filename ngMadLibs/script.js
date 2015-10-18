9e642e45ba218ebfd5b2306b3ddcf21ae84bb86e
Riduan Chitty <rchitty87@gmail.com>
2015-10-18 16:15:27 +0800

    ngMadLibs refactor

--- ngMadLibs/script.js
+++ ngMadLibs/script.js
@@ -1,5 +1,10 @@
 var app = angular.module('madLibsApp', []);
 
 app.controller('mainCtrl', function($scope){
+  $scope.gender = 'male';
+  $scope.submitted = false;
 
+  $scope.submitForm = function(){
+    $scope.submitted = true;
+  }
 });
